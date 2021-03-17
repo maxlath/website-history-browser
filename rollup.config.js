df990c155355316dea272e9b3734166cd350760d
maxlath <k@maxlath.eu>
2021-03-17 12:28:45 +0100

    build background from src directory

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,5 @@
 // Adapted from https://github.com/sveltejs/template/tree/5de3d089
+// via https://gitlab.com/kanthaus/task-lottery/-/blob/main/rollup.config.js
 
 import svelte from 'rollup-plugin-svelte'
 import commonjs from '@rollup/plugin-commonjs'
@@ -7,38 +8,45 @@ import css from 'rollup-plugin-css-only'
 
 const production = !process.env.ROLLUP_WATCH
 
-export default {
-  input: 'src/main.js',
-  output: {
-    sourcemap: true,
-    format: 'iife',
-    name: 'main',
-    file: 'extension/main.js'
-  },
-  plugins: [
-    svelte({
-      compilerOptions: {
-        // enable run-time checks when not in production
-        dev: !production
-      }
-    }),
-    // we'll extract any component CSS out into
-    // a separate file - better for performance
-    css({ output: 'bundle.css' }),
+const build = (entry, dest) => {
+  return {
+    input: entry,
+    output: {
+      sourcemap: true,
+      format: 'iife',
+      name: 'main',
+      file: dest
+    },
+    plugins: [
+      svelte({
+        compilerOptions: {
+          // enable run-time checks when not in production
+          dev: !production
+        }
+      }),
+      // we'll extract any component CSS out into
+      // a separate file - better for performance
+      css({ output: 'bundle.css' }),
 
-    // If you have external dependencies installed from
-    // npm, you'll most likely need these plugins. In
-    // some cases you'll need additional configuration -
-    // consult the documentation for details:
-    // https://github.com/rollup/plugins/tree/master/packages/commonjs
-    resolve({
-      browser: true,
-      dedupe: [ 'svelte' ]
-    }),
+      // If you have external dependencies installed from
+      // npm, you'll most likely need these plugins. In
+      // some cases you'll need additional configuration -
+      // consult the documentation for details:
+      // https://github.com/rollup/plugins/tree/master/packages/commonjs
+      resolve({
+        browser: true,
+        dedupe: [ 'svelte' ]
+      }),
 
-    commonjs(),
-  ],
-  watch: {
-    clearScreen: false
+      commonjs(),
+    ],
+    watch: {
+      clearScreen: false
+    }
   }
 }
+
+export default [
+  build('src/main.js', 'extension/main.js'),
+  build('src/background.js', 'extension/background.js'),
+]
---
+++ src/background.js
@@ -0,0 +1,19 @@
+import { getCurrentTabUrl } from './tabs'
+
+function openPopup () {
+  browser.browserAction.openPopup()
+}
+
+async function openTab () {
+  let url = '/index.html'
+  const currentUrl = await getCurrentTabUrl()
+  if (currentUrl) url += `?url=${encodeURIComponent(currentUrl)}`
+  browser.tabs.create({ url })
+}
+
+browser.commands.onCommand.addListener(command => {
+  if (command === 'open-website-history-browser-popup') openPopup()
+  else if (command === 'open-website-history-browser-tab') openTab()
+})
+
+browser.browserAction.onClicked.addListener(openPopup)
