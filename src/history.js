a81166df2eed2927d92b7877d6fbda42323b650b
maxlath <k@maxlath.eu>
2021-03-19 10:36:36 +0100

    history: extract global title logic

--- src/history.js
+++ src/history.js
@@ -1,5 +1,7 @@
 import { getItemPeriod } from './date'
 import { getBookmarksPerUrl } from './bookmarks'
+import { findGlobalTitle, getShortTitle } from 'title'
+
 const theBeginningOfTimes = new Date(0)
 
 export async function getHistoryItems ({ origin }) {
@@ -35,41 +37,5 @@ const getHistoryItem = async origin => {
   })
   return historyItems.filter(item => item.url.startsWith(origin))
 }
-
-const findGlobalTitle = items => {
-  items = items.filter(item => item.title != null)
-
-  if (items.length < 5) return
-
-  const parts = {}
-  items.forEach(({ title }) => {
-    title.split(partSeparators).slice(1)
-    .forEach(part => {
-      parts[part] = parts[part] || 0
-      parts[part]++
-    })
-  })
-
-  const recurrentParts = Object.keys(parts)
-    .filter(part => parts[part] > 0.9 * items.length)
-
-  const { title: titleWithRecurrentParts } = items.find(item => {
-    return recurrentParts.every(part => item.title.includes(part))
-  })
-
-  const firstRecurrentPartIndex = Math.min(...recurrentParts.map(part => titleWithRecurrentParts.indexOf(part)))
-
-  return titleWithRecurrentParts.slice(firstRecurrentPartIndex)
-}
-
-const getShortTitle = (itemTitle, globalTitle) => {
-  if (!globalTitle) return itemTitle
-  return itemTitle
-  .replace(globalTitle, '')
-  .replace(endSeparators, '')
-}
-
-const partSeparators = /\s*[-—|]{1}\s*/g
-const endSeparators = /(\s*[-—|]{1}\s*)*$/
 
 export const hasBookmarks = item => item.bookmarks != null
---
+++ src/title.js
@@ -0,0 +1,35 @@
+export const findGlobalTitle = items => {
+  items = items.filter(item => item.title != null)
+
+  if (items.length < 5) return
+
+  const parts = {}
+  items.forEach(({ title }) => {
+    title.split(partSeparators).slice(1)
+    .forEach(part => {
+      parts[part] = parts[part] || 0
+      parts[part]++
+    })
+  })
+
+  const recurrentParts = Object.keys(parts)
+    .filter(part => parts[part] > 0.9 * items.length)
+
+  const { title: titleWithRecurrentParts } = items.find(item => {
+    return recurrentParts.every(part => item.title.includes(part))
+  })
+
+  const firstRecurrentPartIndex = Math.min(...recurrentParts.map(part => titleWithRecurrentParts.indexOf(part)))
+
+  return titleWithRecurrentParts.slice(firstRecurrentPartIndex)
+}
+
+export const getShortTitle = (itemTitle, globalTitle) => {
+  if (!globalTitle) return itemTitle
+  return itemTitle
+  .replace(globalTitle, '')
+  .replace(endSeparators, '')
+}
+
+const partSeparators = /\s*[-—|]{1}\s*/g
+const endSeparators = /(\s*[-—|]{1}\s*)*$/
