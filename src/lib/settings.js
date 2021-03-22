061a11bb02d2543ba0adb789301fef24ba6fdb82
maxlath <k@maxlath.eu>
2021-03-23 00:33:12 +0100

    fixup! settings: debounce save

--- src/lib/settings.js
+++ src/lib/settings.js
@@ -3,9 +3,7 @@ import debounce from 'lodash.debounce'
 export const getSettings = () => browser.storage.local.get('settings')
 
 const saveSettings = settings => {
-  console.time('save settings')
   browser.storage.local.set({ settings })
-  console.timeEnd('save settings')
 }
 
 export const lazySaveSettings = debounce(saveSettings, 500)
