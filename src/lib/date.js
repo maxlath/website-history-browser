53c8c64d3c8ac2c831ae8f86be836b4594ae26c9
maxlath <k@maxlath.eu>
2021-03-24 11:36:58 +0100

    date: adjust labels

--- src/lib/date.js
+++ src/lib/date.js
@@ -7,12 +7,12 @@ export const daysAgo = epochTime => Math.floor((Date.now() - epochTime) / oneDay
 export const periods = [
   { thresold: 1, label: 'today', selector: 'last 24 hours' },
   { thresold: 2, label: 'yesterday', selector: 'last 48 hours' },
-  { thresold: 7, label: 'this last week' },
-  { thresold: 30, label: 'this last month' },
-  { thresold: 365, label: 'this last year' },
-  { thresold: 365 * 2, label: 'this last 2 year' },
-  { thresold: 365 * 5, label: 'this last 5 year' },
-  { thresold: 365 * 10, label: 'this last 10 year' },
+  { thresold: 7, label: 'last week' },
+  { thresold: 30, label: 'last month' },
+  { thresold: 365, label: 'last year' },
+  { thresold: 365 * 2, label: 'last 2 year' },
+  { thresold: 365 * 5, label: 'last 5 year' },
+  { thresold: 365 * 10, label: 'last 10 year' },
   { thresold: Infinity, label: 'a while ago', selector: 'all times' },
 ].map(period => {
   period.className = period.label.replace(/\s/g, '-')
